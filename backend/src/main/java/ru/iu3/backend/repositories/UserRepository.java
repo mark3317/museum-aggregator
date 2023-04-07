583160a7f44bc4767a2bb6b09e8b9e772d968157
mark3317 <mark.nokhrin@gmail.com>
2023-04-07 12:43:06 +0300

    part7

--- backend/src/main/java/ru/iu3/backend/repositories/UserRepository.java
+++ backend/src/main/java/ru/iu3/backend/repositories/UserRepository.java
@@ -4,8 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.iu3.backend.models.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository  extends JpaRepository<User, Long>
 {
 
+    Optional<User> findByToken(String s);
+
+    Optional<User> findByLogin(String login);
 }

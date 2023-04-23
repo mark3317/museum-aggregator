435aea1f29fb428766994532064aae32f6efd355
mark3317 <mark.nokhrin@gmail.com>
2023-04-24 00:51:38 +0300

    part9

--- front/src/components/NavigationBar.jsx
+++ front/src/components/NavigationBar.jsx
@@ -3,6 +3,7 @@ import { Navbar, Nav } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom';
+import {Link} from 'react-router-dom'
 
 class NavigationBarClass extends React.Component {
 
@@ -16,16 +17,25 @@ class NavigationBarClass extends React.Component {
     }
 
     render() {
+        let uname = Utils.getUserName();
         return (
             <Navbar bg="light" expand="lg">
-            <Navbar.Brand><FontAwesomeIcon icon={faHome} />{' '}My RPO</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link onClick={() =>{ this.props.navigate("\home")}}>Yet Another Home</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
+                <Navbar.Brand><FontAwesomeIcon icon={faHome} />{' '}My RPO</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link onClick={this.goHome}>Another Home</Nav.Link>
+                        <Nav.Link onClick={() =>{ this.props.navigate("\home")}}>Yet Another Home</Nav.Link>
+                    </Nav>
+                    <Navbar.Text>{uname}</Navbar.Text>
+                    { uname &&
+                    <Nav.Link onClick={this.logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>
+                    }
+                    { !uname &&
+                    <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>
+                    }
+                </Navbar.Collapse>
             </Navbar>
         );
     }
